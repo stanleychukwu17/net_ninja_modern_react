9a76c152fc805fde9c3f4e53cc8d1b7b03b979a4
bigmanstan <stanleychukwu17@gmail.com>
2021-06-04 12:38:04 +0100

    new link added

--- src/navbar.js
+++ src/navbar.js
@@ -4,6 +4,7 @@ const Navbar = () => {
             <div className="logo"><h1>Stanlo bloging;;</h1></div>
             <div className="linkPar">
                 <a href="">Home</a>
+                <a href="">make request</a>
                 <a href="About us">About Us</a>
                 <a href="/contact_us.php">Contact us</a>
             </div>
