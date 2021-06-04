0aca981a9176e84ad4e7e1c501f1b78cd02017e4
bigmanstan <stanleychukwu17@gmail.com>
2021-06-04 10:31:39 +0100

    lots of updates

---
+++ src/navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <div className="navPar">
+            <div className="logo"><h1>Stanlo blog;</h1></div>
+            <div className="linkPar">
+                <a href="">Home</a>
+                <a href="About us">About Us</a>
+                <a href="/">Contact us</a>
+            </div>
+        </div>
+    );
+}
+ 
+export default Navbar;
