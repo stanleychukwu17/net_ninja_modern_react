ba1428cc124d250471b8ffe3621fa9a814877d26
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-07 15:05:25 +0100

    still on the index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import Navbar from './navbar.js';
+import Home from './home.js';
+
+function App() {
+  return (
+    <div className="App_cover">
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root') );
