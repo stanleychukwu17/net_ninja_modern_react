f65492b739fdaf5bc29e18557331bb7a4dc1024a
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-07 15:03:34 +0100

    factorized the index.html page

--- src/navbar.js
+++ src/navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (
         <div className="navPar">
-            <div className="logo"><h1>Stanlo bloging;;</h1></div>
+            <div className="logo"><h1>Stanlo blog</h1></div>
             <div className="linkPar">
                 <a href="">Home</a>
                 <a href="/make.php">make request</a>
