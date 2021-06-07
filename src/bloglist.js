8d3c46c00f2d12c587960044bbe4b378c2e8b2fd
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-07 15:28:38 +0100

    looped the blogs files to show blog in the home page

--- src/bloglist.js
+++ src/bloglist.js
@@ -1,6 +1,15 @@
-const Bloglist = () => {
+const Bloglist = (props) => {
+    const blogs = props.blogs;
+
     return (
-        <div>This is the blog side</div>
+        <div className="mkO_Bg_cvr">
+            <div>All blogs</div>
+            <div>
+                {blogs.map((ech) => (
+                    <div className="" key="{ech.id}">{ech.title}</div>
+                ))}
+            </div>
+        </div>
     );
 }
 
