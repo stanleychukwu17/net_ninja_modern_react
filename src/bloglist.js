8dfe1b269d2d79c4b2b6a6db64feb0086df2559e
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-09 14:14:33 +0100

    improvement of the features of the blog

--- src/bloglist.js
+++ src/bloglist.js
@@ -9,7 +9,7 @@ const Bloglist = (props) => {
                     <div className="mkEchBg" data-id={ech.id} key="{ech.id}">
                         <div><h3>{ech.title}</h3></div>
                         <div>By: {ech.author}</div>
-                        <div><button onClick={props.delF.bind(this, ech.id)}>delete blog</button></div>
+                        <div className="mkEBtn"><button onClick={props.delF.bind(this, ech.id)}>delete blog</button></div>
                     </div>
                 ))}
             </div>
