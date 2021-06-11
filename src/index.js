47907259890aca9cdab458d6e2677ac5d40f8e34
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-11 13:57:16 +0100

    the router components have been imported

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {React} from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './index.css';
 import Navbar from './navbar.js';
