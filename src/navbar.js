3950857b2d582f518dfc7b3006133bf6266b785b
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-11 13:53:34 +0100

    working on navbar links

--- src/navbar.js
+++ src/navbar.js
@@ -7,8 +7,8 @@ const Navbar = () => {
                 <div className="logo"><h1>Stanlo</h1></div>
                 <div className="linkPar">
                     <Link to="/">Home</Link>
-                    <a href="/about_us">About Us</a>
-                    <a href="/new_blog">New blog</a>
+                    <Link href="/about_us">About Us</a>
+                    <Link href="/new_blog">New blog</a>
                 </div>
             </div>
         </Router>
