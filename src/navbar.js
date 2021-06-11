39e7d3f422a45ba9d7e73aca33664861daf59530
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-11 13:49:38 +0100

    navbar1

--- src/navbar.js
+++ src/navbar.js
@@ -2,14 +2,16 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div className="navPar">
-            <div className="logo"><h1>Stanlo</h1></div>
-            <div className="linkPar">
-                <Link to="/">Home</Link>
-                <a href="/about_us">About Us</a>
-                <a href="/new_blog">New blog</a>
+        <Router>
+            <div className="navPar">
+                <div className="logo"><h1>Stanlo</h1></div>
+                <div className="linkPar">
+                    <Link to="/">Home</Link>
+                    <a href="/about_us">About Us</a>
+                    <a href="/new_blog">New blog</a>
+                </div>
             </div>
-        </div>
+        </Router>
     );
 }
  
