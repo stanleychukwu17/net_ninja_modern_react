198518c7a2a0b6c9eccbe291103264d40e15d52b
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-11 13:55:28 +0100

    added the router in the index page

--- src/index.js
+++ src/index.js
@@ -13,13 +13,15 @@ function App() {
   let {data:blogs, pending, error} = Getman('http://localhost:8000/blogs');
 
   return (
-    <div className="App_cover">
-      <Navbar />
-      {error && <div className="errMan">{error}</div>}
-      {pending && <div className="Loading">Loading blog...</div>}
-      {blogs && <Bloglist delF={deleteBlog} blogs={blogs} title="All the blog"/>}
-      {blogs && <Bloglist delF={deleteBlog} blogs={blogs.filter(ech => ech.author === 'mario')} title="Marios blog"/>}
-    </div>
+    <Router>
+      <div className="App_cover">
+        <Navbar />
+        {error && <div className="errMan">{error}</div>}
+        {pending && <div className="Loading">Loading blog...</div>}
+        {blogs && <Bloglist delF={deleteBlog} blogs={blogs} title="All the blog"/>}
+        {blogs && <Bloglist delF={deleteBlog} blogs={blogs.filter(ech => ech.author === 'mario')} title="Marios blog"/>}
+      </div>
+    </Router>
   );
 }
 
