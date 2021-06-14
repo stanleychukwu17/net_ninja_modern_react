60c2877268d5fad58caa466501e36ba957cc3d79
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-14 22:08:06 +0100

    changes

--- src/blog_details.js
+++ src/blog_details.js
@@ -5,18 +5,17 @@ import Getman from './get_content'
 const BlogDetails = (props) => {
     let {id: blog_id} = useParams();
 
-    let dts = Getman('http://localhost:8000/blogs/'+blog_id);
-    console.log(dts);
+    let {data, pending, error} = Getman('http://localhost:8000/blogs/'+blog_id);
 
 
-    if (dts.pending) {
+    if (pending) {
         return ('page loading');
     } else {
         return (
-            <div>
-                <div className="Bgh_title"><h2>Blog details ready</h2></div>
-                <div className="Bgh_author">By: na mano</div>
-                <div className="bgh_body"></div>
+            <div className="container">
+                <div className="Bgh_title"><h2>{data.title}</h2></div>
+                <div className="Bgh_author">By: {data.author}</div>
+                <div className="bgh_body">{data.body}</div>
             </div>
         );
     }
