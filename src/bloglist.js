59cadb9ab4893b7e5c3dcd326ffbda304df4c9bd
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-14 15:33:05 +0100

    changes

--- src/bloglist.js
+++ src/bloglist.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const Bloglist = (props) => {
     const blogs = props.blogs;
 
@@ -7,7 +9,7 @@ const Bloglist = (props) => {
             <div>
                 {blogs.map((ech) => (
                     <div className="mkEchBg" data-id={ech.id} key="{ech.id}">
-                        <div><h3>{ech.title}</h3></div>
+                        <div><h3><Link to="/blogDetails">{ech.title}</Link></h3></div>
                         <div>By: {ech.author}</div>
                         <div className="mkEBtn"><button onClick={props.delF.bind(this, ech.id)}>delete blog</button></div>
                     </div>
--- src/get_content.js
+++ src/get_content.js
@@ -1,22 +1,22 @@
 import {useState, useEffect} from 'react';
 
 const Getman = (url) => {
-    let [obj, setObj] = useState({data:null, pending:true, error:null});
+  let [obj, setObj] = useState({data:null, pending:true, error:null});
 
-    useEffect(() => {
-      setTimeout(() => {
-        fetch(url)
-        .then(jfile => jfile.json())
-        .then(fenal => {
-          setObj({data: fenal, pending:false, error:null});
-        }).catch (err => {
-          setObj({...obj, pending:false, error:err.message});
-        });
-      }, 500);
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(url)
+      .then(jfile => jfile.json())
+      .then(fenal => {
+        setObj({data: fenal, pending:false, error:null});
+      }).catch (err => {
+        setObj({...obj, pending:false, error:err.message});
+      });
+    }, 500);
 
-    }, []);
+  }, [url]);
 
-    return obj;
+  return obj;
 }
  
 export default Getman;
