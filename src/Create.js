184fb413a6972c98d7a9ed4cea150b42df958f7c
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-15 07:39:57 +0100

    changes

--- src/Create.js
+++ src/Create.js
@@ -1,6 +1,17 @@
 const CreateNewBlog = () => {
     return (
-        <div><h2>For creating blog</h2></div>
+        <div>
+            <div><h2>For creating blog</h2></div>
+            <div>
+                <div className=""><p>Title of the blog</p> <p><input type="text" /></p></div>
+                <div className=""><p>Body of blog</p> <p><textarea></textarea></p></div>
+                <div className="">
+                    <p>Author of blog</p>
+                    <p><textarea></textarea></p>
+                </div>
+                <div><button className="btn_blue">Save blog</button></div>
+            </div>
+        </div>
     );
 }
  
