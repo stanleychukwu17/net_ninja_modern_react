00d01e10c6e4116e69dcada0ffac441806dd7b9d
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-15 08:14:29 +0100

    changes

--- src/Create.js
+++ src/Create.js
@@ -1,24 +1,26 @@
-import {useState} from './react';
+import {useState} from 'react';
 
 const CreateNewBlog = () => {
     let [title, setTitle] = useState('john is good');
+    let [author, setAuthor] = useState('stanley');
+    let [body, setBody] = useState('for me man!');
 
     return (
         <div className="frm_Cvr">
             <div className="frm_hdr"><h2>For creating blog</h2></div>
             <div className="frm_inj2">
-                <div className="frm_cnt"><p>Title of the blog</p> <p><input type="text" /></p></div>
+                <div className="frm_cnt"><p>Title of the blog</p> <p><input type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}} /></p></div>
                 <div className="frm_cnt">
                     <p>Author of blog</p>
                     <p>
-                        <select id="">
+                        <select value={author} onChange={(e)=>{setAuthor(e.target.value)}}>
                             <option value="mario">mario</option>
                             <option value="ben">ben</option>
                             <option value="stanley">stanley</option>
                         </select>
                     </p>
                 </div>
-                <div className="frm_cnt"><p>Body of blog</p> <p><textarea></textarea></p></div>
+                <div className="frm_cnt"><p>Body of blog</p> <p><textarea value={body} onChange={(e)=>{setBody(e.target.value)}}></textarea></p></div>
                 <div className="frm_btn"><button className="btn_blue">Save blog</button></div>
             </div>
         </div>
