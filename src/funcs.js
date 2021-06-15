65c0fe2451bd3d7a213163b74bab05bd51c88a1b
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-15 09:16:16 +0100

    changes

--- src/funcs.js
+++ src/funcs.js
@@ -1,8 +1,17 @@
 const DelF = (blog_id) => {
     fetch('http://localhost:8000/blogs/'+blog_id, { method: 'DELETE'});
-    console.log('deleted')
 }
 
-let ojemba = {DelF};
+const NoPage = () => {
+    return (
+        <div>
+            <div><h1>No page found</h1></div>
+            <div><img src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/03135659/Lhasa-Apso-puppies-laying-side-by-side.jpg" alt="" /></div>
+        </div>
+    );
+}
+ 
+
+let ojemba = {DelF, NoPage};
  
 export default ojemba;
